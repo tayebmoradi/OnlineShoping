a542d6a3651fd9952b95452a571df849ded419f3
tayebmoradi <tayeb.mora69@gmail.com>
2023-07-05 23:34:26 +0330

    add validation

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace Models
     public class Product
     {
         public int ProductId { get; set; }
+        [Required(ErrorMessage = " نام محصول نمی تواند خالی باشد")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "توضیحات محصول نمی تواند خالی باشد")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "بارکد محصول نمی تواند خالی باشد")]
         public string Barecode { get; set; }
+        [Required(ErrorMessage = "قیمت محصول نمی تواند خالی باشد")]
         public double Price { get; set; }
+        [Required(ErrorMessage = "تصویر محصول نمی تواند خالی باشد")]
         public string Photo { get; set; }
     }
 }
--- OnlineShoping/Pages/Products/Edit.cshtml.cs
+++ OnlineShoping/Pages/Products/Edit.cshtml.cs
@@ -6,77 +6,81 @@ using Services.Interface;
 
 namespace OnlineShoping.Pages.Products
 {
-    public class EditModel : PageModel
-    {
-        private readonly IProductRepository _productRepository;
-        private readonly IWebHostEnvironment webHostEnvironment;
+	public class EditModel : PageModel
+	{
+		private readonly IProductRepository _productRepository;
+		private readonly IWebHostEnvironment webHostEnvironment;
 
-        public EditModel(IProductRepository productRepository,IWebHostEnvironment webHostEnvironment)
-        {
-            _productRepository = productRepository;
-            this.webHostEnvironment = webHostEnvironment;
-        }
+		public EditModel(IProductRepository productRepository, IWebHostEnvironment webHostEnvironment)
+		{
+			_productRepository = productRepository;
+			this.webHostEnvironment = webHostEnvironment;
+		}
 
-        [BindProperty]
-        public IFormFile Photo { get; set; }
-        [BindProperty]
-        public int ProductId { get; set; }
-        [BindProperty]
-        public Product Product { get; set; }
-        public void OnGet(int id)
-        {
-            
-            if(id  == 0)
-            {
-                Product = null;
-            }
-           Product =  _productRepository.GetProductById(id);
-        }
-       
-        public IActionResult OnPost(Product product)
-        {
-            
-            if(Photo != null)
-            {
-                if(product.Photo != null)
-                {
-                    string FileFolder =
-                    Path.Combine(webHostEnvironment.WebRootPath, "css",product.Photo);
-                    System.IO.File.Delete(FileFolder);
+		[BindProperty]
+		public IFormFile Photo { get; set; }
+		[BindProperty]
+		public int ProductId { get; set; }
+		[BindProperty]
+		public Product Product { get; set; }
+		public void OnGet(int id)
+		{
 
-                }
-                Product.Photo = ProcessUploadFile();
-            }
-            if (Product.ProductId == 0)
-            {
-                Product = _productRepository.AddProduct(Product);
-                return RedirectToPage("index");
-            }
-            else
-            {
-               
-              Product =  _productRepository.UpdateProduct(Product);
-              return RedirectToPage("index");
-              
-            }
-            
-        }
-        private string ProcessUploadFile()
-        {
-            string uniqueFileName = null;
+			if (id == 0)
+			{
+				Product = null;
+			}
+			Product = _productRepository.GetProductById(id);
+		}
 
-            if (Photo != null)
-            {
-                string UploadsFolder =
-                    Path.Combine(webHostEnvironment.WebRootPath, "css");
-                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
-                string filePath = Path.Combine(UploadsFolder, uniqueFileName);
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    Photo.CopyTo(fileStream);
-                }
-            }
-            return uniqueFileName;
-        }
-    }
+		public IActionResult OnPost()
+		{
+			if (ModelState.IsValid)
+			{
+				if (Photo != null)
+				{
+					if (Product.Photo != null)
+					{
+						string FileFolder =
+						Path.Combine(webHostEnvironment.WebRootPath, "css", Product.Photo);
+						System.IO.File.Delete(FileFolder);
+
+					}
+					Product.Photo = ProcessUploadFile();
+				}
+				if (Product.ProductId == 0)
+				{
+					Product = _productRepository.AddProduct(Product);
+					return RedirectToPage("index");
+				}
+				else
+				{
+
+					Product = _productRepository.UpdateProduct(Product);
+					return RedirectToPage("index");
+
+				}
+			}
+			return Page();
+
+
+		}
+		private string ProcessUploadFile()
+		{
+			string uniqueFileName = null;
+
+			if (Photo != null)
+			{
+				string UploadsFolder =
+					Path.Combine(webHostEnvironment.WebRootPath, "css");
+				uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+				string filePath = Path.Combine(UploadsFolder, uniqueFileName);
+				using (var fileStream = new FileStream(filePath, FileMode.Create))
+				{
+					Photo.CopyTo(fileStream);
+				}
+			}
+			return uniqueFileName;
+		}
+	}
 }
