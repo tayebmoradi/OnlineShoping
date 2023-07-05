7426db3e298f652e6158772ea67db974c646dcb5
tayebmoradi <tayeb.mora69@gmail.com>
2023-07-05 21:11:45 +0330

    add image for index.chtml

--- OnlineShoping/Pages/Products/Details.cshtml.cs
+++ OnlineShoping/Pages/Products/Details.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Services.Interface;
 
 namespace OnlineShoping.Pages.Products
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductRepository _productRepository;
+
+        public DetailsModel(IProductRepository  productRepository)
         {
+            _productRepository = productRepository;
+        }
+        [BindProperty]
+        public Product product { get; set; }
+        public void OnGet(int id)
+        {
+            product = _productRepository.GetProductById(id);
         }
     }
 }
